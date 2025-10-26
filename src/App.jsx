0de95478cdbade8cb69bb9ac79fdcc41a9bef27f
RagniNagpal <ragninagpal@gmail.com>
2025-10-26 15:35:13 +0530

    Navbar,About

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,16 @@ import Work from "./components/Work/Work";
 import Education from "./components/Education/Education";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
-import './App.css'
+import BlurBlob from './components/BlurBlob';
 
-function App() {
-
+const App = () => {
   return (
     <div className="bg-[#050414]">
-      <div>
-        
-      </div>
+
+       <BlurBlob position={{ top: '35%', left: '20%' }} size={{ width: '30%', height: '40%' }} />
+
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+      
       <div className="relative pt-20">
         <Navbar />
         <About />
@@ -26,8 +27,9 @@ function App() {
         <Contact />
         <Footer />
       </div>
-    </div>
-  )
-}
 
-export default App
+    </div>
+  );
+};
+
+export default App;

556e268d29ca96fa5a4db410e2f4f86d0fc7e16c
RagniNagpal <ragninagpal@gmail.com>
2025-11-01 20:36:31 +0530

    project,education

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import {FaFacebook, FaTwitter, FaLinkedin, FaInstagram,FaYoutube} from "react-icons/fa";
 
 const Footer = () => {
+  const handleScroll=(sectionId)=>{
+    const section=document.getElementById(sectionId);
+    if(section){
+      section.scrollIntoView({behaviour:"smooth"});
+    }
+  };
+
   return (
-    <div>
-      Footer
-    </div>
+    <footer className="text-white py-8 px-[12vw] md:px-[7vw] lg:px-[20vw]">
+      <div className="container mx-auto"
+      
+    </footer>
   )
 }
 
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -39,7 +39,7 @@ const Skills = () => (
             transitionSpeed={1000}
             gyroscope={true}
           >
-            <div className="grid grid-cols-2 sm:grid-cols-3 gap-3 w-full">
+            <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 w-full">
               {category.skills.map((skill) => (
                 <div
                   key={skill.name}

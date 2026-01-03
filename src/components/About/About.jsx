961a36b2c9dbe823327934865102865aac2be4a6
RagniNagpal <ragninagpal@gmail.com>
2026-01-03 12:21:25 +0530

    peoject completed

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -48,7 +48,7 @@ const About = () => {
             efficient solutions.
           </p>
           {/* Resume Button */}
-            <a href="/Ragni_CV.pdf"  target="_blank"
+            <a href="/RagniNagpal.pdf"  target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
             style={{
--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -109,7 +109,7 @@ const Work = () => {
                     rel="noopener noreferrer"
                     className="w-1/2 bg-gray-800 hover:bg-purple-800 text-gray-400 lg:px-6 lg:py-2 px-2 py-1 rounded-xl lg:text-xl text-sm font-semibold text-center"
                   >
-                    View Code
+                    {/* View Code */}
                   </a>
                   <a
                     href={selectedProject.webapp}
@@ -117,7 +117,7 @@ const Work = () => {
                     rel="noopener noreferrer"
                     className="w-1/2 bg-purple-600 hover:bg-purple-800 text-white lg:px-6 lg:py-2 px-2 py-1 rounded-xl lg:text-xl text-sm font-semibold text-center"
                   >
-                    View Live
+                    {/* View Live */}
                   </a>
                 </div>
               </div>
